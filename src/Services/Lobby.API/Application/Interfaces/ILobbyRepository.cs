ae2471fb2c1f8c27331b454cef654a2dcb4af098
DESKTOP-DAAB48R\ilya <maslov270396@mail.ru>
2026-09-03 18:37:21 +0700

    Fix Lobby interfaces

---
+++ src/Services/Lobby.API/Application/Interfaces/ILobbyRepository.cs
@@ -0,0 +1,31 @@
+﻿using GameBackend.Services.Lobby.API.Domain;
+
+namespace GameBackend.Services.Lobby.API.Application.Interfaces;
+
+/// <summary>
+/// Интерфейс репозитория лобби.
+/// </summary>
+public interface ILobbyRepository
+{
+    /// <summary>
+    /// Получает лобби по идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор лобби.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Лобби или null.</returns>
+    Task<Lobby?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Получает открытые лобби (Gathering и Bidding).
+    /// </summary>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Коллекция открытых лобби.</returns>
+    Task<IReadOnlyCollection<Lobby>> GetOpenLobbiesAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Сохраняет лобби.
+    /// </summary>
+    /// <param name="lobby">Лобби для сохранения.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    Task SaveAsync(Lobby lobby, CancellationToken cancellationToken = default);
+}
